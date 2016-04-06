eb04000734e1832c90fad828d729b5b7defe5e3f
tslator <theslators@gmail.com>
2016-04-06 11:27:14 -0700

    corrected parameter retrieval in scan sensor

--- src/arlobot/arlobot_bringup/scripts/hal/sensors/scan_sensor.py
+++ src/arlobot/arlobot_bringup/scripts/hal/sensors/scan_sensor.py
@@ -66,8 +66,8 @@ class ScanSensor:
 class LaserScanSensor(ScanSensor):
 
     def __init__(self, name):
-        min_range = rospy.get_param("Laser Min Range")
-        max_range = rospy.get_param("Laser Max Range")
+        min_range = get_param("Laser Min Range")
+        max_range = get_param("Laser Max Range")
         ScanSensor.__init__(self, "laser", name+"_laser", min_range, max_range)
 
     def _get_data(self):
@@ -79,8 +79,8 @@ class UltrasonicScanSensor(ScanSensor):
     _OFFSETS = [60, 30, 0, 330, 300, 45, 0, 315, 240, 210, 180, 150, 120, 225, 180, 135]
 
     def __init__(self):
-        min_range = rospy.get_param("Ultrasonic Min Range")
-        max_range = rospy.get_param("Ultrasonic Max Range")
+        min_range = get_param("Ultrasonic Min Range")
+        max_range = get_param("Ultrasonic Max Range")
         ScanSensor.__init__(self,
                             "ultrasonic_scan",
                             "ultrasonic_array",
@@ -114,8 +114,8 @@ class InfraredScanSensor(ScanSensor):
     __OFFSETS = [330, 30, 45, 15, 345, 315, 330, 30, 150, 210, 225, 195, 165, 135, 150, 210]
 
     def __init__(self):
-        min_range = rospy.get_param("Infrared Min Range")
-        max_range = rospy.get_param("Infrared Max Range")
+        min_range = get_param("Infrared Min Range")
+        max_range = get_param("Infrared Max Range")
         ScanSensor.__init__(self,
                             "infrared_scan",
                             "infrared_array",
