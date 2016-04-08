c8e1f89fbc2c80e7dff685291e137f5fb5037ea9
tslator <theslators@gmail.com>
2016-04-08 10:18:16 -0700

    update

--- src/arlobot/environment/onoffmonitor.py
+++ src/arlobot/environment/onoffmonitor.py
@@ -1,19 +1,30 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
-# The purpose of this script is to monitor the on/off signal from the M4-ATX.  The on/off signal is connected to inputs
-# on both of the Psoc4 boards (because the Psoc4's are 5v tolerant) and then made available via the I2C bus.
-#
-# The on/off signal is a pulse that is
-#     - sent by the M4-ATX power supply when the power switch is turned on and
-#     - sent once a minute after the power switch is turned off.
-#
-# The Psoc4 detects the pulse and set a bit in the
-# Of interested here, is to capture the on/off input when the Raspberry Pi is running so that it can be safely shutdown.
-# Additionally, it would be possible to invoke the PowerPi watchdog to turn off (but its not clear if this is necessary
-# if the Raspberry Pi has already safely shutdown).
-#
+# The purpose of this script is to allow the Raspberry Pi to shutdown gracefully when the Arlobot power switch is turned
+# off.
 # Idea stolen from here: https://www.element14.com/community/docs/DOC-78055/l/adding-a-shutdown-button-to-the-raspberry-pi-b
+#
+# The Arlobot PC uses the M4-ATX power supply which is connected directly to the battery and contains logic to maintain
+# power long enough to allow the PC to shutdown gracefully.  This logic is being leveraged for the Raspberry Pi by
+# connecting the PowerPi to the 12v rail of the M4-ATX which means the Raspberry Pi will power up/down at the same time as
+# the PC.
+#
+# The PowerPi has an opto input pin that is, by default, designed to turn on the PowerPi when voltage (3-9v) is applied.
+# The opto pin can also be monitored from software running on the Raspberry Pi.  The opto input pin is connected to the
+# the 5v rail of the Arlobot power distribution which ensures that the opto pin is asserted when M4-ATX powers on which
+# causes the PowerPi and RaspberryPi to power up.  And, because the 5v rail is connected to the power switch, the opto
+# input pin will 0v when the power switch is turned off allowing a monitor on the RapsberryPi to recognize the change
+# and invoke shutdown.
+#
+# To ensure graceful shutdown on the RaspberryPi a script (below) monitors the opto input pin state via the PowerPi status
+# register (bit 2).  The PowerPi is connected to the I2C bus and exports a number of registers including the status
+# register.  The script will run from /etc/rc.local.
+#
+# Script Basics:
+#    - Run the script automatically via /etc/rc.local
+#    - Open the I2C bus
+#    - Read the status register of the both
 
 
 import sys
@@ -24,16 +35,15 @@ import i2c.py
 
 # Raspberry Pi 2 supports only device 1
 I2C_DEVICE = 1
-PSOC4_1_I2C_ADDR = 0x08
-PSCO4_2_I2C_ADDR = 0x09
-PSOC4_STATUS_REG = 6
-ONOFF_STATUS_BIT = 0x0002
+POWER_PI_I2C_ADDR = 0x21
+STATUS_REG = 2
+STATUS_OPTO = 0x04
 
-def OnOffAsserted():
-    psoc4_1_reg_value = i2c_bus.ReadUint16(PSOC4_1_I2C_ADDR, PSOC4_STATUS_REG)
-    psoc4_2_reg_value = i2c_bus.ReadUint16(PSOC4_2_I2C_ADDR, PSOC4_STATUS_REG)
-
-    return (psoc4_1_reg_value & ONOFF_STATUS_BIT) and (psoc4_2_reg_value & ONOFF_STATUS_BIT)
+def OptoAsserted():
+    status_reg = i2c_bus.ReadUint8(POWER_PI_I2C_ADDR, STATUS_REG)
+    asserted = status_reg & STATUS_OPTO
+    print("Opto Asserted: ", asserted)
+    return (asserted)
 
 try:
     i2c_bus = I2CBus(I2C_DEVICE)
@@ -41,7 +51,7 @@ except RuntimeError:
     print("Unable to instantiate I2C bus device 1")
     sys.exit(1)
 
-while not OnOffAsserted():
+while OptoAsserted():
     time.sleep(1)
 
 os.system("sudo shutdown -h now")
