8f155c316e6e0a905ed7e861a675dbdc0fe7d623
tslator <theslators@gmail.com>
2016-06-12 16:34:58 -0700

    made fixes to i2c and i2c motor test.  its seems that read_i2c_block_data is not supported on this version of smbus

--- src/arlobot/arlobot_bringup/scripts/hal/hw/i2c.py
+++ src/arlobot/arlobot_bringup/scripts/hal/hw/i2c.py
@@ -62,28 +62,38 @@ class I2CBus:
         return value if value < (2**15 - 1) else value - 2**16
 
     def ReadUint32(self, address, offset):
-        value = self._try(lambda : self._smbus.read_i2c_block_data(address, offset, 4))
-        return (value[0] << 24) | (value[1] << 16) | (value[2] << 8) | (value[3])
+        values = self._try(lambda : self._smbus.read_i2c_block_data(address, offset, 4))
+        #return (value[0] << 24) | (value[1] << 16) | (value[2] << 8) | (value[3])
+        return struct.unpack('L', str(bytearray(values)))
 
     def ReadInt32(self, address, offset):
-        value = self._try(lambda : self._smbus.read_i2c_block_data(address, offset, 4))
-        int_value = (value[0] << 24) | (value[1] << 16) | (value[2] << 8) | (value[3])
-        return int_value if int_value < (2**31 - 1) else int_value - 2**32
+        values = self._try(lambda : self._smbus.read_i2c_block_data(address, offset, 4))
+        #int_value = (value[0] << 24) | (value[1] << 16) | (value[2] << 8) | (value[3])
+        #return int_value if int_value < (2**31 - 1) else int_value - 2**32
+        return struct.unpack('l', str(bytearray(values)))
+
+    def ReadFloat(self, address, offset):
+        values = self._try(lambda : self._smbus.read_i2c_block_data(address, offset, 4))
+        float_value = struct.unpack('f', str(bytearray(values)))
+        return float_value
 
     def ReadArray(self, address, offset, num_values, type):
-        type_sizes = {'f' : 4, 'l' : 4, 's' : 2, 'b' : 1 }
+        type_sizes = {'d' : 8, 'f' : 4, 'L' : 4, 'l' : 4, 'H' : 2, 'h' : 2, 'B' : 1, 'b' : 1 }
 
-        # Create a format specifier based on the size.  The format specified the number of float values to convert
+        # Create a format specifier based on the number of values requested.  
+        # All of the values will be read as the same type, e.g., all floats, all long, etc
+        # The format specifies the number of float values to convert
         format = '%s%s' % (num_values, type)
         # Calculate number of bytes to read
-        #   - num_values is the number of elements to read
+        #   - num_values is the number of values to read
         #   - num_bytes is num_values * size of each value
         num_bytes = num_values * type_sizes[type]
         values = self._try(lambda: self._smbus.read_i2c_block_data(address, offset, num_bytes))
-        return struct.pack(format, str(bytearray(values)))
+        return struct.unpack(format, str(bytearray(values)))
 
 if __name__ == "__main__":
     import sys
+    import time
     '''
     Note: I2C device 0 is not supported on Raspberry Pi rev 2
     try:
@@ -95,24 +105,33 @@ if __name__ == "__main__":
     try:
         i2c1 = I2CBus(I2CBus.DEV_I2C_1)
         assert(i2c1 != None)
+        print("Successfully opened I2C /dev/i2c1")
     except I2CBusError as err:
         print("Failed to open I2C device 0")
 
-    i2c1.WriteUint16(int(sys.argv[1]), 0x00, 0xff)
-    value = i2c1.ReadUint16(int(sys.argv[1]), 0)
-    print("ReadUint16 returned: ", value)
-    assert(value == 0xff)
 
-    i2c1.WriteUint16(int(sys.argv[1]), 0x00, 0x7f)
-    value = i2c1.ReadUint16(int(sys.argv[1]), 0)
-    print("ReadUint16 returned: ", value)
-    assert(value == 0x7f)
 
-    value = i2c1.ReadUint32(int(sys.argv[1]), 6)
-    print("ReadUint32 returned:", value)
-    values = i2c1.ReadArray(int(sys.argv[1]), 12)
-    assert(len(values) == 8)
-    print("ReadArray returned:", values)
+    i2c1.WriteInt16(int(sys.argv[1]), 2, 100)
+    i2c1.WriteInt16(int(sys.argv[1]), 4, 110)
+    time.sleep(2)
+    left_speed = i2c1.ReadInt16(int(sys.argv[1]), 10)
+    right_speed = i2c1.ReadInt16(int(sys.argv[1]), 12)
+    print("ReadInt16 (left) returned: ", left_speed)
+    print("ReadInt16 (right) returned: ", right_speed)
 
-    values = i2c1.ReadArray(int(sys.argv[1]), )
-    print("ReadArray")
+    x_dist = i2c1.ReadFloat(int(sys.argv[1]), 14)
+    y_dist = i2c1.ReadFloat(int(sys.argv[1]), 18)
+    heading = i2c1.ReadFloat(int(sys.argv[1]), 22)
+    linear = i2c1.ReadFloat(int(sys.argv[1]), 26)
+    angular = i2c1.ReadFloat(int(sys.argv[1]), 30)
+    print("x dist: ", x_dist)
+    print("y dist: ", y_dist)
+    print("heading: ", heading)
+    print("linear: ", linear)
+    print("angular: ", angular)
+
+    print("Reading 5 float values (20 bytes)")
+    float_values = i2c1.ReadArray(int(sys.argv[1]), 14, 20, 'f')
+    print(float_values)
+
+
--- src/arlobot/arlobot_bringup/scripts/hal/hw/i2cmotortest01.py
+++ src/arlobot/arlobot_bringup/scripts/hal/hw/i2cmotortest01.py
@@ -10,26 +10,27 @@ if __name__ == "__main__":
     except I2CBusError:
         print("Error instantiating I2CBus")
 
+    print("successfully created i2c bus")
     speed = int(sys.argv[1])
 
     print("speed: ", speed)
-    right_start = i2c1.ReadInt32(0x08, 8)
-    left_start = i2c1.ReadInt32(0x09, 8)
-    i2c1.WriteInt16(0x08, 2, speed)
-    i2c1.WriteInt16(0x09, 2, speed)
+    right_start = i2c1.ReadFloat(0x08, 0)
+    left_start = i2c1.ReadInt32(0x08, )
+    #i2c1.WriteInt16(0x08, 2, speed)
+    #i2c1.WriteInt16(0x09, 2, speed)
 
-    start_time = time.time()
+    #start_time = time.time()
 
-    time.sleep(float(sys.argv[2]))
+    #time.sleep(float(sys.argv[2]))
 
-    right_velocity = int(i2c1.ReadInt16(0x08, 12))
-    left_velocity = int(i2c1.ReadInt16(0x09, 12))
-    i2c1.WriteInt16(0x08, 2, 0)
-    i2c1.WriteInt16(0x09, 2, 0)
-    end_time = time.time()
-    right_end = i2c1.ReadInt32(0x08, 8)
-    left_end = i2c1.ReadInt32(0x09, 8)
-    print("distance(m): ", (right_end - right_start) * (end_time - start_time)/1000, " velocity(m/s): ", right_velocity)
-    print("distance(m): ", (left_end - left_start) * (end_time - start_time)/1000, " velocity(m/s): ", left_velocity)
+    #right_velocity = int(i2c1.ReadInt16(0x08, 12))
+    #left_velocity = int(i2c1.ReadInt16(0x09, 12))
+    #i2c1.WriteInt16(0x08, 2, 0)
+    #i2c1.WriteInt16(0x09, 2, 0)
+    #end_time = time.time()
+    #right_end = i2c1.ReadInt32(0x08, 8)
+    #left_end = i2c1.ReadInt32(0x09, 8)
+    #print("distance(m): ", (right_end - right_start) * (end_time - start_time)/1000, " velocity(m/s): ", right_velocity)
+    #print("distance(m): ", (left_end - left_start) * (end_time - start_time)/1000, " velocity(m/s): ", left_velocity)
     
 
