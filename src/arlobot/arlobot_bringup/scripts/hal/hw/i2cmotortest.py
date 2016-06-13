fec5aaf5a4fc614ef8469ca762a312f5fdb81d2b
tslator <theslators@gmail.com>
2016-06-13 16:13:09 -0700

    fixed up motor test via i2c and validated the result.  looking good so far.

--- src/arlobot/arlobot_bringup/scripts/hal/hw/i2cmotortest.py
+++ src/arlobot/arlobot_bringup/scripts/hal/hw/i2cmotortest.py
@@ -19,33 +19,40 @@ if __name__ == "__main__":
     except I2CBusError:
         print("Error instantiating I2CBus")
 
-    speed = 200           # mm/s
+    speed = 0.2           # m/s
     psoc_addr = 0x08      # Psoc I2C device is as 0x08
     control_offset = 0    # register 0 is the control register
     clear_odometry = 0x02 # bit 1 clears odometry
     odom_offset = 14      # odometry offset is 14
-    odom_num_bytes = 20   # each odometry field is a float (4 bytes) and there are 5 fields: x/y dist, heading,
+    odom_num_values = 5   # each odometry field is a float (4 bytes) and there are 5 fields: x/y dist, heading,
                           # linear/angular velocity
-    left_cmd_vel_offset = 2
-    right_cmd_vel_offset = 4
+    linear_cmd_vel_offset = 2
+    angular_cmd_vel_offset = 6
     wait_time = 5
+
+    print("Stopping motors")
+    i2c1.WriteFloat(psoc_addr, linear_cmd_vel_offset, 0)
+    i2c1.WriteFloat(psoc_addr, angular_cmd_vel_offset, 0)
 
     print("Clearing the current odometry ...")
     i2c1.WriteUint16(psoc_addr, control_offset, clear_odometry)
-    x_dist, y_dist, heading, linear_vel, angular_vel = tuple(i2c1.ReadArray(psoc_addr, odom_offset, odom_num_bytes, 'f'))
-    print("x dist: {}, y dist: {}, heading: {}, linear vel: {}, angular vel: {}".format(x_dist, y_dist, heading, linear_vel, angular_vel))
+    x_dist, y_dist, heading, linear_vel, angular_vel = tuple(i2c1.ReadArray(psoc_addr, odom_offset, odom_num_values, 'f'))
+    print("x dist: {}\ny dist: {}\nheading: {}\nlinear vel: {}\nangular vel: {}".format(x_dist, y_dist, heading, linear_vel, angular_vel))
     print("Odometry clear complete.")
 
-    print("Commanding motors to {} mm/s".format(speed))
-    i2c1.WriteInt16(psoc_addr, left_cmd_vel_offset, speed)
-    i2c1.WriteInt16(psoc_addr, right_cmd_vel_offset, speed)
+    print("Commanding motors to {} m/s".format(speed))
+    i2c1.WriteFloat(psoc_addr, linear_cmd_vel_offset, speed)
+    i2c1.WriteFloat(psoc_addr, angular_cmd_vel_offset, 0)
     print("Waiting for motion to complete ...")
 
     time.sleep(wait_time)
 
+    i2c1.WriteFloat(psoc_addr, linear_cmd_vel_offset, 0)
+    i2c1.WriteFloat(psoc_addr, angular_cmd_vel_offset, 0)
+
     print("Motion complete")
 
     print("Reading odometry")
-    x_dist, y_dist, heading, linear_vel, angular_vel = tuple(i2c1.ReadArray(psoc_addr, odom_offset, odom_num_bytes, 'f'))
-    print("x dist: {}, y dist: {}, heading: {}, linear vel: {}, angular vel: {}".format(x_dist, y_dist, heading, linear_vel, angular_vel))
+    x_dist, y_dist, heading, linear_vel, angular_vel = tuple(i2c1.ReadArray(psoc_addr, odom_offset, odom_num_values, 'f'))
+    print("x dist: {}\ny dist: {}\nheading: {}\nlinear vel: {}\nangular vel: {}".format(x_dist, y_dist, heading, linear_vel, angular_vel))
 
