12712b5b185bb3df4ac160f7ad77464a0945f9aa
Tim Slator <theslators@gmail.com>
2016-10-20 22:01:06 +0000

    finalized the psochw module and tests

--- src/arlobot/arlobot_bringup/scripts/hal/hw/psochw.py
+++ src/arlobot/arlobot_bringup/scripts/hal/hw/psochw.py
@@ -1,3 +1,8 @@
+"""
+This module provides an abstraction for accessing the Freesoc hardware via the I2C bus
+It provides accessors for motor, debug, and calibration control and reading status and
+distance sensor data.
+"""
 from __future__ import print_function
 
 import time
@@ -8,6 +13,8 @@ class PsocHwError(Exception):
 
 
 class PsocHw:
+
+    # Address offsets for the Psoc I2C registers
     __DEVICE_CONTROL_OFFSET = 0
     __DEBUG_CONTROL_OFFSET = 2
     __LEFT_WHEEL_VELOCITY_OFFSET = 4
@@ -22,6 +29,7 @@ class PsocHw:
     __HEARTBEAT_OFFSET = 164
 
 
+    # Map of the register functionality and offsets
     __REGISTER_MAP = {#---------- READ/WRITE ------------
                       'DEVICE_CONTROL': __DEVICE_CONTROL_OFFSET,
                       #      - Bit 0: enable / disable the HB25 motors
@@ -65,7 +73,7 @@ class PsocHw:
     __DBG_PIDS_BIT = 0x0002
     __DBG_MOTORS_BIT = 0x0004
     __DBG_ODOM_BIT = 0x0008
-    __CAL_SAMPLE_BIT = 0x0010
+    __DBG_SAMPLE_BIT = 0x0010
 
     __CAL_MOTORS_BIT = 0x0001
     __CAL_PID_GAINS_BIT = 0x0002
@@ -86,15 +94,11 @@ class PsocHw:
         self._address = address
         self._i2c_bus = i2cbus
 
-        # The following are needed as a workaround for the I2C bus issue that returns wildly large values for the odometry
-        # values.  A comparison between I2C and serial and only the I2C bus shows these odd values.  The workaround is to
-        # use the last value in the case where the new value is unusable.  It should be pretty close and it can't really
-        # be worse.
-        self._last_x_dist = 0
-        self._last_y_dist = 0
-        self._last_heading = 0
-        self._last_linear_vel = 0
-        self._last_angular_vel = 0
+        self._debug_mask = 0
+
+    # The following methods are divided into two sections: read-write and read-only
+    # The read-write methods support calls that access the read-write section of the Psoc I2C interface
+    # The read-only methods support calls that access the read-only section of the Psoc I2C interface
 
     #------------ Read / Write ---------------
 
@@ -106,8 +110,12 @@ class PsocHw:
         '''
         self._i2c_bus.WriteUint16(self._address, PsocHw.__REGISTER_MAP['DEVICE_CONTROL'], value)
 
-    def _set_debug_control(self, value):
-        self._i2c_bus.WriteUint16(self._address, PsocHw.__REGISTER_MAP['DEBUG_CONTROL'], value)
+    def _set_debug_control(self, bit, set_clear):
+        if set_clear:
+            self._debug_mask |= bit
+        else:
+            self._debug_mask &= ~bit
+        self._i2c_bus.WriteUint16(self._address, PsocHw.__REGISTER_MAP['DEBUG_CONTROL'], self._debug_mask)
 
     def DisableMotors(self):
         self._set_device_control(PsocHw.DISABLE_MOTORS)
@@ -119,34 +127,19 @@ class PsocHw:
         self._set_device_control(PsocHw.__CONTROL_CLEAR_CALIBRATION_BIT)
 
     def EncoderDebug(self, value=True):
-        bitmask = PsocHw.__DBG_ENCODERS_BIT
-        if not value:
-            bitmask = ~PsocHw.__DBG_ENCODERS_BIT
-        self._set_calibration_control(bitmask)
+        self._set_debug_control(PsocHw.__DBG_ENCODERS_BIT, value)
 
     def PidDebug(self, value=True):
-        bitmask = PsocHw.__DBG_PIDS_BIT
-        if not value:
-            bitmask = ~PsocHw.__DBG_PIDS_BIT
-        self._set_calibration_control(bitmask)
+        self._set_debug_control(PsocHw.__DBG_PIDS_BIT, value)
 
     def MotorDebug(self, value=True):
-        bitmask = PsocHw.__DBG_MOTORS_BIT
-        if not value:
-            bitmask = ~PsocHw.__DBG_MOTORS_BIT
-        self._set_calibration_control(bitmask)
+        self._set_debug_control(PsocHw.__DBG_MOTORS_BIT, value)
 
     def OdometryDebug(self, value=True):
-        bitmask = PsocHw.__DBG_ODOM_BIT
-        if not value:
-            bitmask = ~PsocHw.__DBG_ODOM_BIT
-        self._set_calibration_control(bitmask)
+        self._set_debug_control(PsocHw.__DBG_ODOM_BIT, value)
 
     def SampleDebug(self, value=True):
-        bitmask = PsocHw.__CAL_SAMPLE_BIT
-        if not value:
-            bitmask = ~PsocHw.__CAL_SAMPLE_BIT
-        self._set_calibration_control(bitmask)
+        self._set_debug_control(PsocHw.__DBG_SAMPLE_BIT, value)
 
     def SetSpeed(self, left_speed, right_speed):
         '''
@@ -170,6 +163,9 @@ class PsocHw:
         '''
         return self._i2c_bus.ReadUint16(self._address, PsocHw.__REGISTER_MAP['CALIBRATION_STATUS'])
 
+    def GetDeviceStatus(self):
+        return self._get_device_status()
+
     def GetCalibrationStatus(self):
         '''
         '''
@@ -178,53 +174,196 @@ class PsocHw:
     def MotorsInitialized(self):
         '''
         '''
-        result = self._get_device_status()
-        return result & PsocHw.__DEVICE_STATUS_MOTORS_INIT_BIT
+        result = self._get_device_status() & PsocHw.__DEVICE_STATUS_MOTORS_INIT_BIT
+        return result != 0
 
     def MotorsCalibrated(self):
         '''
         '''
-        result = self._get_calibration_status()
-        return result & PsocHw.__CAL_MOTORS_STATUS_BIT
+        result = self._get_calibration_status() & PsocHw.__CAL_MOTORS_STATUS_BIT
+        return result != 0
 
     def PidsCalibrated(self):
-        result = self._get_calibration_status()
-        return result & PsocHw.__CAL_PID_GAINS_STATUS_BIT
+        result = self._get_calibration_status() & PsocHw.__CAL_PID_GAINS_STATUS_BIT
+        return result != 0
 
-    def IsCalibrated(self):
-        result = self._get_calibration_status()
-        return result == PsocHw.__CAL_MOTORS_STATUS_BIT | PsocHw.__CAL_PID_GAINS_STATUS_BIT
+    def Calibrated(self):
+        '''
+        The robot is considered calibrated if either the motors or pids are calibrated
+        Detailed calibration information is available in the accessors above
+        '''
+        result = self._get_calibration_status() & (PsocHw.__CAL_MOTORS_STATUS_BIT | PsocHw.__CAL_PID_GAINS_STATUS_BIT)
+        return result != 0
 
     def GetOdometry(self):
         odometry = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['ODOMETRY'], 5, 'f')
 
         return odometry
 
-    def GetUlrasonicDistances(self):
+    def GetUltrasonicDistances(self):
         '''
         '''
-        # Each of the values is a short value
-        #front = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['FRONT_INFRARED_DISTANCE'], 8, 'H')
-        #rear = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['REAR_INFRARED_DISTANCE'], 8, 'H')
+        front = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['FRONT_INFRARED_DISTANCE'], 8, 'f')
+        rear = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['REAR_INFRARED_DISTANCE'], 8, 'f')
 
-        #return front + rear
-        return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        return front + rear
 
     def GetInfraredDistances(self):
         '''
         '''
-        # Each of the values is a byte value
-        #front = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['FRONT_ULTRASONIC_DISTANCE'], 8, 'B')
-        #rear = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['REAR_ULTRASONIC_DISTANCE'], 8, 'B')
+        front = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['FRONT_ULTRASONIC_DISTANCE'], 8, 'f')
+        rear = self._i2c_bus.ReadArray(self._address, PsocHw.__REGISTER_MAP['REAR_ULTRASONIC_DISTANCE'], 8, 'f')
 
-        #return front + rear
-        return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        return front + rear
 
     def GetHeartbeat(self):
         '''
         '''
         return self._i2c_bus.ReadUint32(self._address, PsocHw.__REGISTER_MAP['HEARTBEAT'])
 
+
+#---------------------------------------------------------------------------------------------------------------------
+# Module Test
+#---------------------------------------------------------------------------------------------------------------------
+
+def test(psoc):
+    from time import sleep
+    indent = ' '*4
+    def test_read_heartbeat():
+        for i in range(5):
+            print("{}Heartbeat: {}".format(indent, psoc.GetHeartbeat() ))
+            sleep(0.5)
+
+    def test_read_infrared_distances():
+        print("{}Infrared Distances: ".format(indent))
+        for idx, entry in enumerate(psoc.GetInfraredDistances()):
+            print("{}{}: {}".format(indent*2, idx, entry))
+
+    def test_read_ultrasonic_distances():
+        print("{}Ultrasonic Distances: ".format(indent))
+        for idx, entry in enumerate(psoc.GetUltrasonicDistances()):
+            print("{}{}: {}".format(indent*2, idx, entry))
+
+    def test_read_odometry():
+        print("{}Odometry: ".format(indent))
+        for idx, entry in enumerate(psoc.GetOdometry()):
+            print("{}{}: {}".format(indent*2, idx, entry))
+
+    def test_read_status():
+        print("{}Calibrated: {}".format(indent, psoc.Calibrated()))
+        print("{}Pids Calibrated: {}".format(indent, psoc.PidsCalibrated()))
+        print("{}Motors Calibrated: {}".format(indent, psoc.MotorsCalibrated()))
+        print("{}Motors Initialized: {}".format(indent, psoc.MotorsInitialized()))
+        print("{}Get Calibration Status: {:2x}".format(indent, psoc.GetCalibrationStatus()))
+        print("{}Get Device Status: {:2x}".format(indent, psoc.GetDeviceStatus()))
+
+    def test_set_debug():    
+        '''
+        This test turns on/off debug in the Psoc.  To monitor the results, it is necessary to start a second
+        ssh session and open the serial port.  Each test case enables, wait, and disables each debug features
+        '''
+        delay_time = 2.0
+        print("{}Enable encoder debug:".format(indent))
+        psoc.EncoderDebug()
+        sleep(delay_time)
+        print("{}Disable encoder debug:".format(indent))
+        psoc.EncoderDebug(False)
+        
+        print("{}Enable pid debug:".format(indent))
+        psoc.PidDebug()
+        sleep(delay_time)
+        print("{}Disable pid debug:".format(indent))
+        psoc.PidDebug(False)
+        
+        print("{}Enable motor debug:".format(indent))
+        psoc.MotorDebug()
+        sleep(delay_time)
+        print("{}Disable motor debug:".format(indent))
+        psoc.MotorDebug(False)
+        
+        print("{}Enable odometry debug:".format(indent))
+        psoc.OdometryDebug()
+        sleep(delay_time)
+        print("{}Disable odometry debug:".format(indent))
+        psoc.OdometryDebug(False)
+        
+        print("{}Enable sample debug:".format(indent))
+        psoc.SampleDebug()
+        sleep(delay_time)
+        print("{}Disable sample debug:".format(indent))
+        psoc.SampleDebug(False)
+        
+    def test_set_speed():
+        psoc.MotorDebug()
+        print("{}Set left motor speed {:.3f} ...".format(indent, 0.100))
+        psoc.SetSpeed(0.100, 0.0)
+        sleep(2.0)
+
+        print("{}Set right motor speed {:.3f} ...".format(indent, 0.100))
+        psoc.SetSpeed(0.0, 0.100)
+        sleep(2.0)
+
+        print("{}Set left motor speed {:.3f} ...".format(indent, -0.100))
+        psoc.SetSpeed(-0.100, 0.0)
+        sleep(2.0)
+
+        print("{}Set right motor speed {:.3f} ...".format(indent, -0.100))
+        psoc.SetSpeed(0.0, -0.100)
+        sleep(2.0)
+
+        print("{}Set left/right motor speed {:.3f},{:.3f} ...".format(indent, 0.100, 0.100))
+        psoc.SetSpeed(0.100, 0.100)
+        sleep(2.0)
+        
+        print("{}Set left/right motor speed {:.3f},{:.3f} ...".format(indent, 0.100, -0.100))
+        psoc.SetSpeed(0.100, -0.100)
+        sleep(2.0)
+
+        print("{}Set left/right motor speed {:.3f},{:.3f} ...".format(indent, -0.100, 0.100))
+        psoc.SetSpeed(-0.100, 0.100)
+        sleep(2.0)
+
+        print("{}Set left/right motor speed {:.3f},{:.3f} ...".format(indent, -0.100, -0.100))
+        psoc.SetSpeed(-0.100, -0.100)
+        sleep(2.0)
+
+        print("{}Set left/right motor speed {:.3f},{:.3f} ...".format(indent, 0.0, 0.0))
+        psoc.SetSpeed(0.0, 0.0)
+        psoc.MotorDebug(False)
+    
+    def test_set_control():
+        print("{}Clear odometry ...".format(indent))
+        print(psoc.GetOdometry())
+        psoc.ClearOdometry()
+        print(psoc.GetOdometry())
+
+    
+    # Just in case ;-)
+    psoc.SetSpeed(0.0,0.0)
+
+    print("Test Case 1: read heartbeat ...")
+    test_read_heartbeat()
+
+    print("Test Case 2: read infrared distances ...")
+    test_read_infrared_distances()
+
+    print("Test Case 3: read ultrasonic distances ...")
+    test_read_ultrasonic_distances()
+
+    print("Test Case 4: read odometry ...")
+    test_read_odometry()
+
+    print("Test Case 5: read status ...")
+    test_read_status()
+
+    print("Test Case 6: set debug ...")
+    test_set_debug()
+
+    print("Test Case 7: set speed ...")
+    test_set_speed()
+    
+    print("Test Case 8: set control ...")
+    test_set_control()
 
 if __name__ == "__main__":
     from i2c import I2CBus, I2CBusError
@@ -238,46 +377,7 @@ if __name__ == "__main__":
     except PsocHwError:
         print("Failed to create PsocHw instance")
 
-    # Test speed minimum value
-    psoc.SetSpeed(0.1, 0)
-    time.sleep(2.0)
-    odometry = tuple(psoc.GetOdometry())
-    print("psoc.GetOdometry: {} {} {} {}".format(*odometry))
-    #assert(result == -100)
-
+    psoc.SetSpeed(0.0, 0.0)
     
-    # Test speed median value
-    psoc.SetSpeed(0, 0)
-    time.sleep(2.0)
-    odometry = tuple(psoc.GetOdometry())
-    print("psoc.GetOdometry: {} {} {} {}".format(*odometry))
-    #assert(result == 0)
+    test(psoc)
 
-    # Test speed maximum value
-    psoc.SetSpeed(-0.1, 0)
-    time.sleep(2.0)
-    odometry = tuple(psoc.GetOdometry())
-    print("psoc.GetOdometry: {} {} {} {}".format(*odometry))
-    #assert(result == 100)
-
-    psoc.SetSpeed(0, 0)
-
-
-    # Test read device status
-    result = psoc._get_status()
-    print("{:x}".format(result))
-
-    # Test read odometry
-    odom = psoc.GetOdometry()
-    print(odom)
-
-    # Test getting infrared distances
-    distances = psoc.GetInfraredDistances()
-    print(distances)
-
-    # Test getting ultrasonic distances
-    distances = psoc.GetUlrasonicDistances()
-    print(distances)
-
-    status = psoc._get_status()
-    print("{:x}".format(status))
