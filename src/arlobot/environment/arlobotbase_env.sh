c33f96f5bcdb62abdb7d72654f7d89dfc51d0807
Tim Slator <theslators@gmail.com>
2016-11-09 16:56:32 +0000

    environment tweaks

--- src/arlobot/environment/arlobotbase_env.sh
+++ src/arlobot/environment/arlobotbase_env.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-source arlobotbase.conf
-source env.conf
-source $ARLOBOT_CATKIN_ROOT/devel/setup.bash
+#source arlobotbase.conf
+#source env.conf
+#source $ARLOBOT_CATKIN_ROOT/devel/setup.bash
+
+export ROS_MASTER_URI=http://arlobot:11311
+export ROS_IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+export ROS_HOSTNAME=arlobotbase
+source /home/arlo/catkin_ws/devel/setup.bash
 
 exec "@"
+
+
