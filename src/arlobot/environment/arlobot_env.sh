0c115e4a3589c3a4d64e1030b6791ced8d5e68c6
Tim Slator <theslators@gmail.com>
2016-11-09 17:35:42 +0000

    environment and remote invocation updates

--- src/arlobot/environment/arlobot_env.sh
+++ src/arlobot/environment/arlobot_env.sh
@@ -4,4 +4,4 @@ source arlobot.conf
 source env.conf
 source $ARLOBOT_CATKIN_ROOT/devel/setup.bash
 
-exec "@"
+exec "$@"
--- src/arlobot/environment/arlobotbase_env.sh
+++ src/arlobot/environment/arlobotbase_env.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-#source arlobotbase.conf
-#source env.conf
-#source $ARLOBOT_CATKIN_ROOT/devel/setup.bash
+source ~/arlobot.conf
+source arlobotbase.conf
+source $ARLOBOT_CATKINWS_ROOT/devel/setup.bash
 
-export ROS_MASTER_URI=http://arlobot:11311
-export ROS_IP=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-export ROS_HOSTNAME=arlobotbase
-source /home/arlo/catkin_ws/devel/setup.bash
-
-exec "@"
+exec "$@"
 
 
