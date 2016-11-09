ba6fa3ea8381319395258b02e8d18d9d3268a21c
tslator <theslators@gmail.com>
2016-11-08 18:29:44 -0800

    made updates for launching remote nodes on arlobotbase

---
+++ src/arlobot/environment/env.sh
@@ -0,0 +1,5 @@
+# Source this file from .bashrc and from the *_env.sh files
+export ARLOBOT_CATKIN_ROOT=/home/arlo/arlobot_catkin_ws
+export ARLOBOTBASE_CATKIN_ROOT=/home/arlo/catkin_ws
+export ARLOBOT_ROOT=/home/arlo/catkin_ws/src/arlobot
+export ARLOBOT_ENV=$ARLOBOT_ROOT/src/arlobot/environment
