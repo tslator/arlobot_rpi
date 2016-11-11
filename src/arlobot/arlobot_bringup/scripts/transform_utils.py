232b5b27385d1f6ab9c5da3991b673acb56442fb
tslator <theslators@gmail.com>
2016-11-11 02:02:38 -0800

    fix up in drive node

--- src/arlobot/arlobot_bringup/scripts/transform_utils.py
+++ src/arlobot/arlobot_bringup/scripts/transform_utils.py
@@ -14,6 +14,7 @@ def quat_to_angle(quat):
     rot = PyKDL.Rotation.Quaternion(quat.x, quat.y, quat.z, quat.w)
     return rot.GetRPY()[2]
 
+
 def normalize_angle(angle):
     res = angle
     while res > pi:
@@ -21,4 +22,22 @@ def normalize_angle(angle):
     while res < -pi:
         res += 2.0 * pi
     return res
+
+
+def uni2diff(self, v, w, L, R):
+    two_v = 2 * v
+    w_l = w * L
+    two_r = 2 * R
+
+    l_v = (two_v - w_l) / two_r
+    r_v = (two_v + w_l) / two_r
+
+    return l_v, r_v
+
+
+def diff2uni(self, l_v, r_v, L, R):
+    v = (l_v + r_v) * (R / 2)
+    w = (r_v - l_v) * (R / L)
+
+    return v, w
 
